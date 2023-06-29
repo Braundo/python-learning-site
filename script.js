808a0f4405a42055c18185f54fa5b3858158f903
Braundo <aaronbraundmeier@gmail.com>
2023-06-28 20:28:14 -0500

    localstorage changes

--- script.js
+++ script.js
@@ -10,14 +10,13 @@ $(document).ready(function () {
 
     $("#progress-bar div").css("width", progress + "%");
 
-// Save checkbox states to localStorage
-$(".sub-checkbox").each(function () {
-  var checkboxId = $(this).attr("id");
-  var isChecked = $(this).prop("checked");
-  localStorage.setItem(checkboxId, isChecked);
-});
+    // Save checkbox states to localStorage
+    $(".sub-checkbox").each(function () {
+      var checkboxId = $(this).attr("id");
+      var isChecked = $(this).prop("checked");
+      localStorage.setItem(checkboxId, isChecked);
+    });
   }
-
 
   $("#reset-button").click(function () {
     $(".top-level-checkbox").prop("checked", false);
@@ -40,7 +39,6 @@ $(".sub-checkbox").each(function () {
     updateProgressBar();
     updateContentBlockTransparency($(this).closest(".bullet"));
   });
-  
 
   $(".top-level-checkbox").change(function () {
     var isChecked = $(this).prop("checked");
@@ -63,29 +61,31 @@ $(".sub-checkbox").each(function () {
     $contentBlock.css("opacity", allTopLevelCheckboxesChecked ? 0.5 : 1);
   }
 
+  document.addEventListener("DOMContentLoaded", function () {
+    var body = document.body;
+    var icon = document.getElementById("dark-mode-icon");
+
+    // Initialize icon based on initial dark mode state
+    icon.innerHTML = body.classList.contains("dark-mode")
+      ? "&#9728;"
+      : "&#9790;";
+  });
+
+  function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+
+    var darkModeToggle = document.querySelector(".dark-mode-toggle");
+    darkModeToggle.classList.toggle("dark");
+  }
+
+  const checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+  for (const checkbox of checkboxes) {
+    const checkboxId = checkbox.id;
+    const isChecked = localStorage.getItem(checkboxId) === "true";
+    checkbox.checked = isChecked;
+  }
+
   updateProgressBar();
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  var body = document.body;
-  var icon = document.getElementById("dark-mode-icon");
-
-  // Initialize icon based on initial dark mode state
-  icon.innerHTML = body.classList.contains("dark-mode") ? "&#9728;" : "&#9790;";
-});
-
-function toggleDarkMode() {
-  var body = document.body;
-  body.classList.toggle("dark-mode");
-
-  var darkModeToggle = document.querySelector(".dark-mode-toggle");
-  darkModeToggle.classList.toggle("dark");
-}
-
-const checkboxes = document.querySelectorAll("input[type='checkbox']");
-
-for (const checkbox of checkboxes) {
-  const checkboxId = checkbox.id;
-  const isChecked = localStorage.getItem(checkboxId) === "true";
-  checkbox.checked = isChecked;
-}
